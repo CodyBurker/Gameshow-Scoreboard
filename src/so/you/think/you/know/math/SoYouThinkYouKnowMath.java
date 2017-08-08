63ce712412b46da4c4d407ab3d58ba44348429e2
Cody Burker <codyburker@gmail.com>
2017-08-08 18:24:48 -0400

    Changed file names

--- src/so/you/think/you/know/math/SoYouThinkYouKnowMath.java
+++ src/so/you/think/you/know/math/SoYouThinkYouKnowMath.java
@@ -1,8 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+// Cody Burker
+// August 2017
 package so.you.think.you.know.math;
 
 import javafx.application.Application;
@@ -10,17 +7,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import so.you.think.you.know.math.*;
 
-/**
- *
- * @author cody
- */
 public class SoYouThinkYouKnowMath extends Application {
-    
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("PrimaryWindow.fxml"));
         
         Scene scene = new Scene(root);
         
