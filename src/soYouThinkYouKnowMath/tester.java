177ac0d7dc416958b6881e3fd07e7f58b618fcda
Cody Burker <codyburker@gmail.com>
2017-08-08 21:19:05 -0400

    Added "tester.java" to debug classes Game.java, Player.java, and Slide.java

---
+++ src/soYouThinkYouKnowMath/tester.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soYouThinkYouKnowMath;
+
+/**
+ *
+ * @author cody
+ */
+public class tester {
+    public static void main(String[] args) {
+        System.out.println("Testing custom classes Game, Player, and Slide.");
+    }
+}
