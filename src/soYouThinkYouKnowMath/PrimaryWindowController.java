e0c6cdd2c0259273f9c8e37370c30e6e6b915028
Cody Burker <codyburker@gmail.com>
2017-08-09 00:39:00 -0400

    Started Adding Methods/Variables to Buttons/Fields

--- src/soYouThinkYouKnowMath/PrimaryWindowController.java
+++ src/soYouThinkYouKnowMath/PrimaryWindowController.java
@@ -22,6 +22,8 @@ import javafx.scene.control.Label;
 import javafx.scene.control.SingleSelectionModel;
 import javafx.scene.control.Tab;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
@@ -30,21 +32,77 @@ import javafx.stage.WindowEvent;
  *
  * @author cody
  */
-
-
 public class PrimaryWindowController implements Initializable {
+
     Stage stage;
     SecondaryWindowController secondaryWindow;
     //@Override
-    
-    @FXML
-    private Button up;
-    
+
     @FXML
     private TabPane tabPane;
 
-    
+    @FXML
+    private Button p1Up;
+    @FXML
+    private Button p2Up;
+    @FXML
+    private Button p3Up;
+    @FXML
+    private Button p4Up;
+    @FXML
+    private Button p5Up;
+    @FXML
+    private Button p6Up;
+    @FXML
+    private Button p1Down;
+    @FXML
+    private Button p2Down;
+    @FXML
+    private Button p3Down;
+    @FXML
+    private Button p4Down;
+    @FXML
+    private Button p5Down;
+    @FXML
+    private Button p6Down;
+    @FXML
+    private TextField p1Name;
+    @FXML
+    private TextField p2Name;
+    @FXML
+    private TextField p3Name;
+    @FXML
+    private TextField p4Name;
+    @FXML
+    private TextField p5Name;
+    @FXML
+    private TextField p6Name;
+    @FXML
+    private TextField p1Institution;
+    @FXML
+    private TextField p2Institution;
+    @FXML
+    private TextField p3Institution;
+    @FXML
+    private TextField p4Institution;
+    @FXML
+    private TextField p5Institution;
+    @FXML
+    private TextField p6Institution;
+    @FXML
+    private ToggleButton p1Show;
+    @FXML
+    private ToggleButton p2Show;
+    @FXML
+    private ToggleButton p3Show;
+    @FXML
+    private ToggleButton p4Show;
+    @FXML
+    private ToggleButton p5Show;
+    @FXML
+    private ToggleButton p6Show;
     Game game;
+
     public void initialize(URL url, ResourceBundle rb) {
         try {
             FXMLLoader loader = new FXMLLoader();
@@ -64,20 +122,43 @@ public class PrimaryWindowController implements Initializable {
         stage.setOnCloseRequest((WindowEvent we) -> {
             System.exit(0);
         });
-        
+
         // Open into edit pane...
         SingleSelectionModel<Tab> selectionModel;
         selectionModel = tabPane.getSelectionModel();
         selectionModel.select(1);
-        
+
         game = new Game();
-        game.players.add(new Player("Player 1","Franklin"));
+        game.players.add(new Player("Player 1", "Franklin"));
         secondaryWindow.loadGame(game);
         secondaryWindow.update();
-        game.players.add(new Player("Cody","Franklin"));
-        game.slides.add(new Slide("/imagePath",1));
+        game.players.add(new Player("Cody", "Franklin"));
+        game.slides.add(new Slide("/imagePath", 1));
         secondaryWindow.update();
+
+    }
+    @FXML
+    private void p1UpClick(ActionEvent event){
+    }
+    @FXML
+    private void p2UpClick(ActionEvent event){
         
     }
-    
+    @FXML
+    private void p3UpClick(ActionEvent event){
+        
+    }
+    @FXML
+    private void p4UpClick(ActionEvent event){
+        
+    }
+    @FXML
+    private void p5UpClick(ActionEvent event){
+        
+    }
+    @FXML
+    private void p6UpClick(ActionEvent event){
+        
+    }
+
 }
